a5b1eb7d51d7bb2cc7c8a1b45cac9a92ba14e725
Daymeion <Daymeion Hughes>
2025-07-03 21:50:33 -0700

    Fix next.config.ts: move PWA config out of nextConfig object for next-pwa v5+ compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
 import withPWA from 'next-pwa';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  pwa: {
-    dest: 'public',
-    disable: isDev,
-  },
-};
-
-export default withPWA(nextConfig);
+export default withPWA({
+  dest: 'public',
+  disable: isDev,
+});
