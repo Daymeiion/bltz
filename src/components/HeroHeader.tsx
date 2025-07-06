9c7ce4e994db336774e8ac451b484174bc9fb5ac
Daymeion <Daymeion Hughes>
2025-07-06 15:56:21 -0700

    Remove settings button from HeroHeader and ensure height is 300px

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Cog6ToothIcon } from '@heroicons/react/24/outline';
+// import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 
 interface HeroHeaderProps {
   backgroundImage: string;
@@ -10,7 +10,7 @@ interface HeroHeaderProps {
 
 export default function HeroHeader({ backgroundImage, cardImage, profileImage, alt }: HeroHeaderProps) {
   return (
-    <div className="relative w-full h-[400px] flex items-center justify-center overflow-hidden">
+    <div className="relative w-full h-[300px] flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <Image
         src={backgroundImage}
@@ -20,29 +20,24 @@ export default function HeroHeader({ backgroundImage, cardImage, profileImage, a
         className="z-0"
         priority
       />
-      {/* Settings Icon */}
-      <div className="absolute top-4 right-4 z-20 bg-black/60 rounded-full p-2">
-        <Cog6ToothIcon className="w-6 h-6 text-white" />
-      </div>
-      {/* Card and Profile Images Centered at Bottom */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[260px] h-[320px] z-10">
-        <Image
-          src={cardImage}
-          alt="Card"
-          width={260}
-          height={320}
-          className="absolute bottom-0 left-1/2 -translate-x-1/2 drop-shadow-xl"
-          priority
-        />
-        <Image
-          src={profileImage}
-          alt={alt || 'Profile'}
-          width={180}
-          height={220}
-          className="absolute left-1/2 -translate-x-1/2 bottom-0 z-10"
-          priority
-        />
-      </div>
+      {/* Card Image Absolutely Bottom Center */}
+      <Image
+        src={cardImage}
+        alt="Card"
+        width={260}
+        height={320}
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 drop-shadow-xl z-10"
+        priority
+      />
+      {/* Profile Image Absolutely Bottom Center, Overlaying Card */}
+      <Image
+        src={profileImage}
+        alt={alt || 'Profile'}
+        width={180}
+        height={220}
+        className="absolute left-1/2 -translate-x-1/2 bottom-0 z-20"
+        priority
+      />
     </div>
   );
 } 
